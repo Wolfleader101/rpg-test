602afb66f6692abce928118b39a41dc222dc33ba
Wolfleader <james.morgan@iinet.net.au>
2021-07-19 11:04:24 +0800

    look into using scriptable object potentially in the future. for now no need

--- Assets/Scripts/Player/Stats/BaseStat.cs
+++ Assets/Scripts/Player/Stats/BaseStat.cs
@@ -2,17 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseStat : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+// [CreateAssetMenu(fileName = "Stat", menuName = "ScriptableObjects/Stat")]
+// public class BaseStat : ScriptableObject
+// {
+//     #region Stat Bar
+//
+//     [Header("Stats Bars")]
+//
+//     [SerializeField] private StatsBar statBar;
+//     public StatsBar StatBar => statBar;
+//     
+//     #endregion
+//
+//     #region Base Value
+//
+//     [Header("Base Stats")] 
+//     [SerializeField] private float baseValue = 100f;
+//     public float BaseValue => baseValue;
+//
+//     #endregion
+//
+//     #region Passive Recovery
+//
+//     [Header("Passive Recovery")]
+//     [SerializeField] private float recoveryPerSecond = 4f;
+//
+//     public bool canRecover = true;
+//
+//     #endregion
+//
+//     #region Current Stat
+//
+//     [Header("Current Stats")]
+//     
+//     private float _currentValue = 0f;
+//     public float CurrentValue
+//     {
+//         get => _currentValue;
+//         set
+//         {
+//             _currentValue = value;
+//             statBar.SetValue(CurrentValue);
+//         }
+//     }
+//     
+//     #endregion
+//
+//     #region Max Stat
+//
+//     [Header("Max Stats")] 
+//     private float _maxValue = 0f;
+//     public float MaxValue
+//     {
+//         get => _maxValue;
+//         set
+//         {
+//             _maxValue = value;
+//             statBar.SetMaxValue(_maxValue);
+//         }
+//     }
+//     #endregion
+//
+//     #region Stat Buff
+//
+//     [field: Header("Stat Buff")]
+//     public float StatBuff { get; set; } = 0f;
+//
+//     #endregion
+//
+// }
