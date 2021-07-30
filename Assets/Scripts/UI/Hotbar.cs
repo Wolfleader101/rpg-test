328ac67e55fcf97b4f793ca88e45a743ae09c6aa
Wolfleader <james.morgan@iinet.net.au>
2021-07-30 16:08:01 +0800

    made it easier to read

--- Assets/Scripts/UI/Hotbar.cs
+++ Assets/Scripts/UI/Hotbar.cs
@@ -35,16 +35,13 @@ public class Hotbar : MonoBehaviour
 
     private void AddItem(BaseItem item, int itemCount)
     {
-        // if it can't increment then add it to next slot
-        // to do this i will implement a queue system
-        // it will add an item to the queue
-        // on next for each check if the slot is empty AND THERE IS QUEUED ITEM/S
-        // if so add it to that slot and remove it from queued items
-        // otherwise continue
-        // if by end of the foreach loop there is no slot for it,
-        // then drop item
-        List<Dictionary<BaseItem, int>> queuedItems = new List<Dictionary<BaseItem, int>>();
+        // list of hotbar slots
         var hotbarItems = gameObject.GetComponentsInChildren<HotbarItem>().ToList();
+
+        // this is for splitting items
+        var queuedItems = new List<Dictionary<BaseItem, int>>();
+
+        // if itemCount is greater than maxStackSize then it will need to split them
         while (itemCount > item.MaxStackSize)
         {
             queuedItems.Add(new Dictionary<BaseItem, int>()
@@ -53,10 +50,12 @@ public class Hotbar : MonoBehaviour
                     item, item.MaxStackSize
                 }
             });
+            
             Debug.Log($"{itemCount} Added Queued Item {item.MaxStackSize}");
-            itemCount = itemCount - item.MaxStackSize;
+            itemCount -= item.MaxStackSize;
         }
 
+        // Add in the remainder
         queuedItems.Add(new Dictionary<BaseItem, int>()
         {
             {
@@ -64,48 +63,42 @@ public class Hotbar : MonoBehaviour
             }
         });
 
-
+        // loop over each inventory slot
         foreach (var button in hotbarItems)
         {
+            // if slot is empty
             if (button.currentItem == null)
             {
-                // Debug.Log($"Queued item count : {queuedItems.Count}");
                 foreach (var queuedItem in queuedItems.Where(queuedItem => queuedItem.ContainsKey(item)))
                 {
-                    // Debug.Log($"Queued item name {queuedItem[item]}");
                     button.AddItem(item, queuedItem[item]);
                     queuedItems.Remove(queuedItem);
                     break;
                 }
-
                 continue;
             }
 
-            if (button.currentItem == item && button.itemCount < button.currentItem.MaxStackSize)
+            // if slots item is not current item AND if its already at max cap
+            if (button.currentItem != item && button.itemCount >= button.currentItem.MaxStackSize) continue;
+            
+            foreach (var queuedItem in queuedItems)
             {
-                if (queuedItems.Count > 0)
+                var incrementRem = button.IncrementCount(queuedItem[item]);
+                if (incrementRem == 0)
                 {
-                    foreach (var queuedItem in queuedItems)
-                    {
-                        var incrementRem = button.IncrementCount(queuedItem[item]);
-                        if (incrementRem == 0)
-                        {
-                            queuedItems.Remove(queuedItem);
-                            break;
-                        }
-
-                        queuedItem[item] = incrementRem;
-                        break;
-                    }
+                    queuedItems.Remove(queuedItem);
+                    break;
                 }
-                
+
+                queuedItem[item] = incrementRem;
+                break;
             }
         }
 
         // if item could not be added
         // drop the item
         //DropItem(Item, itemCount);
-        
+
         // remove count from Inventory
         inventory.RemoveItem(item, itemCount);
     }
