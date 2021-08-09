5169118029a81cba281b58290788e9455b47329d
Wolfleader <james.morgan@iinet.net.au>
2021-08-09 20:35:51 +0800

    ai now follows you

--- Assets/VisionAgent.cs
+++ Assets/VisionAgent.cs
@@ -9,6 +9,7 @@ public class VisionAgent : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private float visionConeArc;
     [SerializeField] private float lineOfSightDistance = 100f;
+    [SerializeField] private float turnSpeed = 100f;
 
     private bool _hasLos = false;
 
@@ -24,16 +25,18 @@ public class VisionAgent : MonoBehaviour
     {
         Vector2 targetPos = target.transform.position;
 
+        
+        Debug.DrawRay(transform.position, transform.up, Color.red);
         if (InVisionCone(targetPos) && HasLos(targetPos))
         {
+            FacePoint(targetPos);
             this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            
         }
         else
         {
             this.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
         }
-        
-        
     }
 
     private bool InVisionCone(Vector2 point)
@@ -42,21 +45,21 @@ public class VisionAgent : MonoBehaviour
         Vector2 ourPos = transform.position;
         Vector2 dirToPoint = point - ourPos;
 
-        float angle =  Vector2.Angle(dirToPoint, fwd);
+        float angle = Vector2.Angle(dirToPoint, fwd);
 
         return angle <= visionConeArc / 2.0;
     }
 
-    private bool HasLos(Vector2 point)
+    private bool HasLos(Vector3 point)
     {
         _checkThisFrame = !_checkThisFrame;
 
         if (!_checkThisFrame) return _hasLos;
 
         var rayPoint = transform.InverseTransformPoint(point);
-        // rayPoint.y = Mathf.Abs(rayPoint.y);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayPoint, lineOfSightDistance);
-        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, point - transform.position, lineOfSightDistance);
+        Debug.DrawRay(transform.position, point - transform.position, Color.cyan);
+
         if (hit.collider != null && (hit.collider.gameObject.layer == LayerMask.NameToLayer("Player")))
         {
             _hasLos = true;
@@ -68,6 +71,19 @@ public class VisionAgent : MonoBehaviour
 
         return _hasLos;
     }
-    
 
+    private void FacePoint(Vector3 point)
+    {
+        var localPoint = transform.InverseTransformPoint(point);
+        var turnDir = Mathf.Sign(localPoint.x);
+        var turnAmount = (turnSpeed * Time.deltaTime);
+        var angle = Vector3.Angle(Vector3.forward, localPoint);
+
+        if (angle < turnAmount)
+        {
+            turnAmount = angle;
+        }
+
+        transform.Rotate(Vector3.forward, -turnAmount * turnDir);
+    }
 }
