bcc7a19392817ae1e43fbfc0ab03ae2617a0848b
Wolfleader <james.morgan@iinet.net.au>
2021-08-21 11:19:35 +0800

    improved editor

--- Assets/Scripts/Editor/LootTableEditor.cs
+++ Assets/Scripts/Editor/LootTableEditor.cs
@@ -11,6 +11,7 @@ public class LootTableEditor : Editor
 {
     private BaseLootTable lootTableComp;
     private static bool showItems = false;
+    private static GUILayoutOption buttonWidth = GUILayout.Width(100);
 
     void OnEnable()
     {
@@ -32,22 +33,31 @@ public class LootTableEditor : Editor
         {
             for (int i = 0; i < lootTableComp.items.Count; i++)
             {
+                EditorGUILayout.BeginHorizontal();
                 lootTableComp.items[i] = (LootTableItem)EditorGUILayout.ObjectField(lootTableComp.items[i], typeof(LootTableItem), false);
+                if (GUILayout.Button("Remove Item", buttonWidth))
+                {
+                    lootTableComp.items.Remove(lootTableComp.items[i]);
+                }
+                EditorGUILayout.EndHorizontal();
                 lootTableComp.items[i].MinDrop = EditorGUILayout.IntSlider("Min Drop", lootTableComp.items[i].MinDrop, 1, lootTableComp.items[i].MaxDrop - 1);
                 lootTableComp.items[i].MaxDrop = EditorGUILayout.IntSlider("Max Drop", lootTableComp.items[i].MaxDrop, lootTableComp.items[i].MinDrop, 100);
                 lootTableComp.items[i].DropChance = EditorGUILayout.Slider("DropChance", lootTableComp.items[i].DropChance, 0, 1);
                 // max value should be based on the element size and all their values
+                
+                GUILayout.Space(20);
             }
+            
+            GUILayout.Space(10);
 
-            if (GUILayout.Button("Add Item"))
+            if (GUILayout.Button("Add Item", buttonWidth))
             {
                 //LootTableItem item = (LootTableItem) EditorGUILayout.ObjectField(null, typeof(LootTableItem), false);
                 lootTableComp.items.Add(ScriptableObject.CreateInstance<LootTableItem>());
             }
-            
-
         }
 
+        EditorUtility.SetDirty(lootTableComp);
         
 
         
